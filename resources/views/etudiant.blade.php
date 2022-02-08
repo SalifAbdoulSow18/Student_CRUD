4f814b5f5249c63b476686b2311671ee5a9458ae
SalifAbdoulSow18 <abdoulsalifsow06@gmail.com>
2022-02-08 15:33:55 +0000

    la gestion de l'interface et des liens

--- resources/views/etudiant.blade.php
+++ resources/views/etudiant.blade.php
@@ -1,12 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Liste des Etudiants</h1>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('content')
+
+<div class="my-3 p-3 bg-body rounded shadow-sm">
+    <h3 class="border-bottom pb-2 mb-4">Liste des Etudiants Incrits</h3>
+    
+    <div class="mt-4">
+        <div class="d-flex justify-content-end mb-3">
+            <a href="#" class="btn btn-primary">Ajouter un Etudiant</a>
+        </div>
+        <table class="table table-bordered table-strip table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nom</th>
+                    <th scope="col">Prenom</th>
+                    <th scope="col">Classe</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>
+                        <a href="#" class="btn btn-info">Editer</a>
+                        <a href="#" class="btn btn-danger">Supprimer</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+  </div>
+
+@endSection
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('accueil');
 
-Route::get('/etudiant', [EtudiantController::class, "index"]);
+Route::get('/etudiant', [EtudiantController::class, "index"])->name('etudiant');
